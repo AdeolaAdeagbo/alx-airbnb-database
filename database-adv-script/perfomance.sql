a38756fe5f89896c7ad77582f299d6f94097c6ce
AdeolaAdeagbo <adeagbozain50@gmail.com>
2025-09-05 15:07:18 +0100

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,4 +1,4 @@
--- Initial query: retrieves all bookings with user, property, and payment details
+-- Initial query retrieving all bookings with user, property, and payment details
 SELECT 
     b.booking_id,
     b.start_date,
@@ -29,6 +29,41 @@ JOIN Property p ON b.property_id = p.property_id
 JOIN Payment pay ON b.booking_id = pay.booking_id
 WHERE b.status = 'confirmed'
   AND b.start_date >= '2025-01-01';
+
+-- Analyze performance using EXPLAIN
+EXPLAIN
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.status,
+    b.total_price,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    u.phone_number,
+    u.role,
+    u.created_at AS user_created_at,
+    p.property_id,
+    p.name AS property_name,
+    p.description,
+    p.location,
+    p.pricepernight,
+    p.created_at AS property_created_at,
+    p.updated_at AS property_updated_at,
+    pay.payment_id,
+    pay.amount AS payment_amount,
+    pay.payment_date,
+    pay.payment_method
+FROM Booking b
+JOIN User u ON b.user_id = u.user_id
+JOIN Property p ON b.property_id = p.property_id
+JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed'
+  AND b.start_date >= '2025-01-01';
+
+
 
 
 
