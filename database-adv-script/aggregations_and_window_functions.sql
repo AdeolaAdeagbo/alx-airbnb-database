01f3e0d0d8a31265621ff8cfe0280aa92ff54032
AdeolaAdeagbo <adeagbozain50@gmail.com>
2025-09-05 14:27:42 +0100

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,9 +1,21 @@
-SELECT user_id, COUNT(*) AS TOTAL_BOOKINGS
+-- Total number of bookings per user
+SELECT user_id, COUNT(*) AS total_bookings
 FROM Booking
 GROUP BY user_id;
 
+
+-- Rank properties based on total bookings using RANK()
 SELECT property_id, 
        COUNT(*) AS total_bookings,
        RANK() OVER (ORDER BY COUNT(*) DESC) AS property_rank
 FROM Booking
 GROUP BY property_id;
+
+
+-- Rank properties based on total bookings using ROW_NUMBER()
+SELECT property_id, 
+       COUNT(*) AS total_bookings,
+       ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS property_row_number
+FROM Booking
+GROUP BY property_id;
+
